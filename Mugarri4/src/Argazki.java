458d108fd138b975418357fa4017b1ea3057718a
oaginagabo23wg <146086565+oaginagabo23wg@users.noreply.github.com>
2024-04-26 13:54:46 +0200

    Update Argazki.java

--- Mugarri4/src/Argazki.java
+++ Mugarri4/src/Argazki.java
@@ -2,19 +2,19 @@ import java.sql.*;
 
 public class Argazki {
     public int idArgazki;
-    public   String izenburua;
-    public Date data;
-    public  String fitxategia;
+    //public   String izenburua;
+    //public Date data;
+   // public  String fitxategia;
     public  int bistaratzeKop;
-    public   int idArgazkilari;
+    //public   int idArgazkilari;
 
-    public Argazki(int idArgazki, String izenburua, Date data, String fitxategia, int bistaratzeKop, int idArgazkilari) {
+    public Argazki(int idArgazki, /*String izenburua, Date data, String fitxategia,*/ int bistaratzeKop /*,int idArgazkilari*/) {
         this.idArgazki = idArgazki;
-        this.izenburua = izenburua;
-        this.data = data;
-        this.fitxategia = fitxategia;
+        //this.izenburua = izenburua;
+        //this.data = data;
+        //this.fitxategia = fitxategia;
         this.bistaratzeKop = bistaratzeKop;
-        this.idArgazkilari = idArgazkilari;
+        //this.idArgazkilari = idArgazkilari;
     }
 
 }
