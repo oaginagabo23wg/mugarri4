2514d3ac6d2aa609f92afa10dfb180b71e1cc3b4
oaginagabo23wg <146086565+oaginagabo23wg@users.noreply.github.com>
2024-05-08 22:06:21 +0200

    Update Argazkilari.java

--- Mugarri4/src/Argazkilari.java
+++ Mugarri4/src/Argazkilari.java
@@ -1,8 +1,32 @@
 import java.sql.*;
 public class Argazkilari {
+    private int idArgazkilari;
+    private String izena;
+    private boolean saritua;
 
-    public Argazkilari() {
+    public Argazkilari(int idArgazkilari, String izena, boolean saritua) {
+        this.idArgazkilari = idArgazkilari;
+        this.izena = izena;
+        this.saritua = saritua;
     }
+
+    @Override
+    public String toString() {
+        return this.izena;
+    }
+
+    public int getIdArgazkilari() {
+        return idArgazkilari;
+    }
+
+    public String getIzena() {
+        return izena;
+    }
+
+    public boolean isSaritua() {
+        return saritua;
+    }
+
     public String[] argazkilariakLortu(){
         String url = "jdbc:mysql://localhost/Mugarri4";
         String erab = "root";
