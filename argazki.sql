60d4ede6305db7ad60ac956a687fcd080f69afe2
oaginagabo23wg <146086565+oaginagabo23wg@users.noreply.github.com>
2024-05-09 22:40:59 +0200

    Update argazki.sql

--- argazki.sql
+++ argazki.sql
@@ -1,4 +1,4 @@
-create database Argazki;
+
 create table Argazkiak ( 
  idArgazki integer,
  izenburua varchar(50),
@@ -12,11 +12,11 @@ create table Argazkiak (
  izena varchar(59),
  saritua boolean
  );
- insert into argazkiak values (1,"ansealdams1","2000-01-01","ansealdams1.jpg",0,1);
-insert into argazkiak values (2,"ansealdams2","2000-01-02","ansealdams2.jpg",0,1);
-insert into argazkiak values (3,"rothko1","2000-01-03","rothko1.jpg",0,2);
-insert into argazkiak values (4,"vangogh1","2000-01-04","vangogh1.jpg",0,3);
-insert into argazkiak values (5,"vangogh2","2000-01-05","vangogh2.jpg",0,3);
+insert into argazkiak values (1,"ansealdams1","2024-05-01","./irudiak/ansealdams1.jpg",0,1);
+insert into argazkiak values (2,"ansealdams2","2024-05-02","./irudiak/ansealdams2.jpg",0,1);
+insert into argazkiak values (3,"rothko1","2024-05-03","./irudiak/rothko1.jpg",0,2);
+insert into argazkiak values (4,"vangogh1","2024-05-04","./irudiak/vangogh1.jpg",0,3);
+insert into argazkiak values (5,"vangogh2","2024-05-05","./irudiak/vangogh2.jpg",0,3);
 insert into argazkilari values (1,"ansealdams",true);
 insert into argazkilari values (2,"rothko",false);
 insert into argazkilari values (3,"vangogh",true);
