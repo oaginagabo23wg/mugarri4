8a6dbdd35e0486176cea32f1eac8c6a627f38657
oaginagabo23wg <146086565+oaginagabo23wg@users.noreply.github.com>
2024-05-09 22:44:43 +0200

    Update Argazkilari.java

--- Mugarri4/src/Argazkilari.java
+++ Mugarri4/src/Argazkilari.java
@@ -1,30 +1,40 @@
 import java.sql.*;
-public class Argazkilari {
-    private int idArgazkilari;
-    private String izena;
-    private boolean saritua;
 
-    public Argazkilari(int idArgazkilari, String izena, boolean saritua) {
+public class Argazki {
+    public int idArgazki;
+    public   String izenburua;
+    public Date data;
+    public  String fitxategia;
+    public  int bistaratzeKop;
+    public   int idArgazkilari;
+
+    public Argazki(int idArgazki, String izenburua, Date data, String fitxategia, int bistaratzeKop, int idArgazkilari) {
+        this.idArgazki = idArgazki;
+        this.izenburua = izenburua;
+        this.data = data;
+        this.fitxategia = fitxategia;
+        this.bistaratzeKop = bistaratzeKop;
         this.idArgazkilari = idArgazkilari;
-        this.izena = izena;
-        this.saritua = saritua;
     }
 
     @Override
     public String toString() {
-        return this.izena;
+        return izenburua;
     }
 
     public int getIdArgazkilari() {
         return idArgazkilari;
     }
 
-    public String getIzena() {
-        return izena;
+    public int getBistaratzeKop() {
+        return bistaratzeKop;
     }
 
-    public boolean isSaritua() {
-        return saritua;
+    public void setBistaratzeKop(int bistaratzeKop) {
+        this.bistaratzeKop = bistaratzeKop;
     }
 
+    public Date getData() {
+        return data;
+    }
 }
